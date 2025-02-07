af4bfab65672d544ce10bdb07e42af1b128632cc
TharukaJayawarna <jayawarnatharuka@gmail.com>
2025-02-07 15:33:53 +0530

    third commit

--- src/main/java/com/zerocode/hotelPackagesApi/model/Customer.java
+++ src/main/java/com/zerocode/hotelPackagesApi/model/Customer.java
@@ -17,7 +17,6 @@ public class Customer {
     private String password;
     private String email;
     private String profilePhotoUrl;
-    
 
     @OneToOne
     private LoyaltyPoints loyaltyPoints;
